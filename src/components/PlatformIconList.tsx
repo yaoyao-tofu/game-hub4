1c68126c5bf1c99ac30928384029fa6359b77e32
yaoyao-tofu <maningyi8@gmail.com>
2025-03-07 21:21:39 -0800

    Create generic data fetching hook

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -32,8 +32,8 @@ const PlatformIconList = ({ platforms }: Props) => {
 
   return (
     <HStack marginY={1}>
-      {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      {platforms.map((platform, index) => (
+        <Icon key={index} as={iconMap[platform.slug]} color="gray.500" />
       ))}
     </HStack>
   );
